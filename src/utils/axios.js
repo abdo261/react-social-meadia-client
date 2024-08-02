d33252903824c07b430fc51322cd02992b9267e4
Obsku <abdellah.basketeur2015@gmail.com>
2024-08-02 19:29:48 +0100

    change axioas token to Barear tokn

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 const userInfo = JSON.parse(localStorage.getItem('user_info'));
 export const token = userInfo ? userInfo?.token : null;
 export const request = axios.create({
-    baseURL:process.env.REACT_APP_API_URL,headers:{token}
+    baseURL:process.env.REACT_APP_API_URL,headers:{ Authorization: token ? `Bearer ${token}` : ''}
 })
 
      
