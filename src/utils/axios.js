c5c280313b54e3a0f646ce9663e1dd0c33eed389
Obsku <abdellah.basketeur2015@gmail.com>
2024-08-02 19:26:12 +0100

    modify token axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
-export const request = axios.create({
-    baseURL:process.env.REACT_APP_API_URL,headers:{Authorization:localStorage.getItem('user_info').token}
-})
 const userInfo = JSON.parse(localStorage.getItem('user_info'));
-export const token = userInfo ? userInfo.token : null;
+export const token = userInfo ? userInfo?.token : null;
+export const request = axios.create({
+    baseURL:process.env.REACT_APP_API_URL,headers:{token}
+})
+
      
